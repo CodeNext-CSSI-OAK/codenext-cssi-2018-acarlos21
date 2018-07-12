6246c47bf416bfb36afae30e56fafc4a50ca9d4f
Student <student@Admins-MacBook-Air.local>
2018-07-12 10:44:57 -0700

    update

--- haunted-house.js
+++ haunted-house.js
@@ -10,7 +10,7 @@ console.log("You are standing in front of a spooky haunted house.");
 console.log("You hear what sounds like screaming coming from inside the house.");
 
 let enterHouse = readline.question("Will you enter the house? (yes or no) ");
-if(enterHouse == "y" || enterHouse == "yes") {
+if(enterHouse === "y" || enterHouse === "yes") {
   console.log("You walk into the quiet house with a fear of getting a jump scare from something like a ghost");
 } else {
   console.log("You are clearly too courious to walk away, so you go to see what is going on in the house");
@@ -19,18 +19,18 @@ if(enterHouse == "y" || enterHouse == "yes") {
 //-----------
 
 let mainHallway = readline.question("You see stairs and you see a door at the end of the hall. Where do you go?(stairs or hall?)");
-if(mainHallway == "stairs"){
+if(mainHallway === "stairs"){
   console.log("You go us the stairs and you think you see something.");
 
 
-}else if(mainHallway == "hall"){
+}else if(mainHallway === "hall"){
   console.log("The door that you went into was the door to the living room. You see a body on the floor.");
 }
 //-----------
 
-if(mainHallway =="stairs"){
+if(mainHallway ==="stairs"){
   let upstairs = readline.question("Will you go investigate or avoid it?(investigate or avoid?)");
-  if(mainHallway == "stairs" && upstairs == "investigate"){
+  if(mainHallway === "stairs" && upstairs === "investigate"){
     console.log("That thing that you saw was a man with a bloody shirt. He murders you.");
     console.log("Thanks for playing!");
   } else {
@@ -41,12 +41,12 @@ if(mainHallway =="stairs"){
 
 //----------
 
-if(mainHallway =="hall"){
-  let livingRoom = readline.question("What do you do?(call the cops or not say anything?)");
-  if(mainHallway == "hall" && livingRoom == "call the cops"){
+if(mainHallway ==="hall"){
+  let livingRoom = readline.question("What do you do?(cops or nothing?)");
+  if(mainHallway === "hall" && livingRoom === "cops"){
     console.log("The police come and search the house and find that the murderer was upstairs. You helped put a guity man behind bars");
     console.log("Thanks for playing!");
-  } else if(mainHallway == "hall" && livingRoom == "not say anything"){
+  } else if(mainHallway === "hall" && livingRoom === "nothing"){
     console.log("It has been a week since the death and the cops tested the house just to find your fingerprints on the door and in the house. You spend life in prison.");
     console.log("Moral of the story is that you shouldn't touch anything");
     console.log("Thanks for playing!");
